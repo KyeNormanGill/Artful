432d9a0ab4406f122683a6429a75866dba7b62a1
KyeNormanGill <kyenormangill@tafensw.net.au>
2017-12-16 03:47:42 +1100

    hurrdurr my names dragon loli and i look at docs

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,9 +4,9 @@ const { MessageAttachment } = require('discord.js');
 module.exports = {
 	name: 'avatar',
 	run: (client, message, args) => {
-		if (!args) return message.channel.send({ files: [new MessageAttachment(message.author.displayAvatarURL({ format: 'png', size: 1024 }), 'file.jpg')] });
+		if (!args) return message.channel.send(new MessageAttachment(message.author.displayAvatarURL({ format: 'png', size: 1024 }), 'file.jpg'));
 		const user = parseUser(message, args);
 		if (!user) return;
-		return message.channel.send({ files: [new MessageAttachment(user.displayAvatarURL({ format: 'png', size: 1024 }), 'file.jpg')] });
+		return message.channel.send(new MessageAttachment(user.displayAvatarURL({ format: 'png', size: 1024 }), 'file.jpg'));
 	}
 }
