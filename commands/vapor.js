6605f9cb90159cb2556675620d64c7a080eaf6a4
KyeNormanGill <kyenormangill@tafensw.net.au>
2017-12-27 20:18:22 +1100

    vaporwave

---
+++ commands/vapor.js
@@ -0,0 +1,18 @@
+function _charToFullWidth(char) {
+	const c = char.charCodeAt(0);
+	return c >= 33 && c <= 126
+		? String.fromCharCode((c - 33) + 65281)
+		: char;
+}
+
+function _stringToFullWidth(string) {
+	return string.split('').map(_charToFullWidth).join('');
+}
+
+module.exports = {
+	name: 'ping',
+	run: (client, message, args) => {
+		if (!args) return;
+		return message.edit(_stringToFullWidth(args));
+	}
+}
